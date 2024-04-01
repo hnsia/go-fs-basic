366db7cffd571b3f7d6a87baeb1d828c82760baa
hnsia <44498178+hnsia@users.noreply.github.com>
2024-04-01 23:36:24 +0800

    feat: add middlewares

--- backend/main.go
+++ backend/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"database/sql"
+	"net/http"
+
 	// 	"encoding/json"
 	"log"
 	// 	"net/http"
 	"os"
-	// 	"github.com/gorilla/mux"
+
+	"github.com/gorilla/mux"
 	// 	"github.com/lib/pq"
 )
 
@@ -31,4 +34,44 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// create router
+	router := mux.NewRouter()
+	router.HandleFunc("/api/go/users", getUsers(db)).Methods("GET")
+	router.HandleFunc("/api/go/users", createUsers(db)).Methods("POST")
+	router.HandleFunc("/api/go/users/{id}", getUser(db)).Methods("GET")
+	router.HandleFunc("/api/go/users/{id}", updateUser(db)).Methods("PUT")
+	router.HandleFunc("/api/go/users/{id}", deleteUsers(db)).Methods("DELETE")
+
+	// wrap the router with CORS and JSON content type middlewares
+	enhancedRouter := enableCORS(jsonContentTypeMiddleware(router))
+
+	// start server
+	log.Fatal(http.ListenAndServe(":8000", enhancedRouter))
+}
+
+func enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Set CORS headers
+		w.Header().Set("Access-Control-Allow-Origin", "*") // Allow any origin
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		// Check if the request is for CORS preflight
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		// Pass down the request to the next middleware (or final handler)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func jsonContentTypeMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Set JSON Content-Type
+		w.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
 }
